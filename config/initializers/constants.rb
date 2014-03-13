16f6f46d563e848276a0b675b77ca7c28bcef515
Xifip <costelljohn@gmail.com>
2014-03-13 20:20:55 +0000

    mod initalizer

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,5 @@
 #config/initializers/constants.rb
+s3_config_file = File.join(Rails.root,'config','amazon_s3.yml')
+raise "#{s3_config_file} is missing!" unless File.exists? s3_config_file
+
 S3_CONFIG = YAML.load_file("#{::Rails.root}/config/amazon_s3.yml")
