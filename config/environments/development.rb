b01e101f404982c27fd4a05c394f165e3e749772
Xifip <costelljohn@gmail.com>
2014-03-18 09:52:00 +0000

    added email3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,19 @@ Som::Application.configure do
 
  # Change mail delvery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :letter_opener
-  
+  # Change mail delvery to either :smtp, :sendmail, :file, :test  
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "gmail.com",
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: "costelljohn@gmail.com",
+  #  password: 
+  #}
+
   #devise mailer in development
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
