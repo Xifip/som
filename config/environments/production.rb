b4c885044e11411e3d1deda9b635d02f7f4de563
Xifip <costelljohn@gmail.com>
2014-03-18 09:02:55 +0000

    added email2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,18 +20,35 @@ Som::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Change mail delvery to either :smtp, :sendmail, :file, :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "costelljohn@gmail.com",
-    password: ENV['email_pw']
+  require 'tlsmail'    
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,  
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :tls                  => true,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => 'costelljohn@gmail.com',
+    :password           => ENV['email_pw']
   }
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
+  #  config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "gmail.com",
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: "costelljohn@gmail.com",
+  #  password: ENV['email_pw']
+  #}
 
   # Specify what domain to use for mailer URLs
   config.action_mailer.default_url_options = {host: "secret-dusk-8986.herokuapp.com"}
