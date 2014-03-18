934b6ba831605750e94f4ab26f6426bb7de7c299
Xifip <costelljohn@gmail.com>
2014-03-18 08:55:00 +0000

    added email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,22 @@ Som::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "costelljohn@gmail.com",
+    password: ENV['email_pw']
+  }
+
+  # Specify what domain to use for mailer URLs
+  config.action_mailer.default_url_options = {host: "secret-dusk-8986.herokuapp.com"}
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
